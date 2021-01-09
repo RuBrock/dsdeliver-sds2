69d196d517ed2c3e83aed9d19196c9bc01a3132b
Rubens Brock <r.brocksilva@yahoo.com.br>
2021-01-09 14:02:59 -0300

    Add Home page

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -1,13 +1,11 @@
 import './styles.scss';
 import { ReactComponent as Logo } from '../../assets/images/logo.svg';
 
-const Navbar = () => {
-    return (
-        <nav className="main-navbar">
-            <Logo />
-            <a href="home" className="logo-text">DS Delivery</a>
-        </nav>
-    );
-}
+const Navbar = () => (
+    <nav className="main-navbar">
+        <Logo />
+        <a href="home" className="logo-text">DS Delivery</a>
+    </nav>
+);
 
 export default Navbar;
